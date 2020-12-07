15797e749e455bc894e35d4da3ae6418e513c75e
Santiago-Ezequiel-Valdez <fluit105@gmail.com>
2020-12-07 03:04:19 -0300

    Se ha agregado la funcionalidad de se detecte, alerte al usuario y le solicite una nueva entrada en caso de que la contraseña ingresada no cumpla con el formato que se solicita

--- Modulo Administracion.cpp
+++ Modulo Administracion.cpp
@@ -181,6 +181,9 @@ void rAsi() {
 						printf("--> ERROR: El ID de usuario debe de tener al menos dos letras mayusculas\n");
 						printf("**************************************************************************\n\n\n\n\n");
 						system("pause");
+						allow = 0;
+						uppers = 0;
+						digits = 0;
 					} else {
 						allow++;
 					}
@@ -191,6 +194,9 @@ void rAsi() {
 						printf("--> ERROR: El ID de usuario no puede tener más de 3 digitos\n");
 						printf("**************************************************************************\n\n\n\n\n");
 						system("pause");
+						allow = 0;
+						uppers = 0;
+						digits = 0;
 					} else {
 						allow++;
 					}
@@ -230,10 +236,141 @@ void rAsi() {
 			}
 		} while (err == 1);
 		
-		printf("\nIngrese una contraseña: ");
-		_flushall();
-		gets(usr.password);
-		printf("Ingrese el apellido y nombre: ");
+		do {
+			err = 0;
+			system("cls");
+			printf("\n\n----------------------------------------------------------------------------\n\n\n");
+			printf("*-*-*-*-*-*-            Registro de nuevo Asistente             -*-*-*-*-*-*\n\n\n");
+			printf("----------------------------------------------------------------------------\n\n\n\n");
+			SetConsoleTextAttribute(hConsole, 15);
+			printf("-->Ingrese una contraseña: <--\n\n");
+			SetConsoleTextAttribute(hConsole, 8);
+			printf("Formato\n");
+			printf("-Entre 6 y 32 caracteres de largo\n");
+			printf("-Poseer al menos una letra mayúscula y una minúscula\n");
+			printf("-Poseer al menos un número\n");
+			printf("-No tener más de tres letras y números en orden creciente consecutivo\n\n");
+			SetConsoleTextAttribute(hConsole, 15);
+			printf("-Entrada: ");
+			_flushall();
+			gets(usr.password);
+		
+			if(strlen(usr.password) > 32 || strlen(usr.password) < 6) {
+				err = 1;
+				system("cls");
+				printf("\n\n\n**************************************************************************\n");
+				printf("--> La contraseña debe tener un largo de entre 6 y 10 caracteres\n");
+				printf("**************************************************************************\n\n\n\n\n");
+				system("pause");
+			} else {
+					int uppers=0;
+					int lowers=0;
+					int digits=0;
+					for (int i = 0; i < strlen(usr.password); i++) {
+						if(isupper(usr.password[i]) && isalpha(usr.password[i])) {
+							uppers++;
+						}
+						if(islower(usr.password[i]) && isalpha(usr.password[i])) {
+							lowers++;
+						}
+						if(isdigit(usr.password[i])) {
+							digits++;
+						}
+					}
+			
+					if(uppers == 0) {
+						err = 1;
+						system("cls");
+						printf("\n\n\n**************************************************************************\n");
+						printf("--> ERROR: La contraseña debe de tener al menos una letra mayúscula\n");
+						printf("**************************************************************************\n\n\n\n\n");
+						system("pause");
+				
+					}
+					if(lowers == 0) {
+						err = 1;
+						system("cls");
+						printf("\n\n\n**************************************************************************\n");
+						printf("--> ERROR: La contraseña debe de tener al menos una letra minúscula\n");
+						printf("**************************************************************************\n\n\n\n\n");
+						system("pause");
+					
+					}
+					if(digits == 0) {
+						err = 1;
+						system("cls");
+						printf("\n\n\n**************************************************************************\n");
+						printf("--> ERROR: La contraseña debe tener al menos 1 dígito\n");
+						printf("**************************************************************************\n\n\n\n\n");
+						system("pause");
+			
+					}
+				}
+			if(err == 0) {
+				int numero[3];
+				int aux;
+				int count = 0;
+				for(int i = 0; i < strlen(usr.password) ; i++) {
+			  		if(isdigit(usr.password[i])) {
+			  			numero[count] = usr.password[i];
+			  			count++;
+					}
+				}
+
+				if(numero[0] + 1 == numero[1]){
+					if(numero[0] + 2 == numero[2]){
+						if(numero[0] + 3 == numero[3]) {
+							err = 1;
+							system("cls");
+							printf("\n\n\n**************************************************************************\n");
+							printf("--> ERROR: La contraseña no debe tener mas de 3 digitos consecutivos.\n");
+							printf("**************************************************************************\n\n\n\n\n");
+							system("pause");
+							
+							numero[3] = 0;
+						} else {break;}
+					} else {break;}
+				} else {break;}
+			}
+			if(err == 0){
+				char letra[32];
+				for(int i = 0; i < strlen(usr.password); i++) {
+					if(isalpha(letra[i])){
+						letra[i] = usr.password[i];
+						letra[i] = toupper(letra[i]);
+					}
+					if(i > 0){
+						if(letra[i-1] + 1 == letra[i]){
+									err = 1;
+									system("cls");
+									printf("\n\n\n**************************************************************************\n");
+									printf("--> ERROR: La contraseña no debe ser alfabeticamente consecutiva.\n");
+									printf("**************************************************************************\n\n\n\n\n");
+									system("pause");
+									break;
+						}
+					}
+				}
+				for(int j = 0; j < strlen(usr.password); j++){
+					if(usr.password[j] == '.' || usr.password[j] == ',' || usr.password[j] == ';' || usr.password[j] == '"'|| usr.password[j] == '(' || usr.password[j] == ')' || usr.password[j] == '¿' || usr.password[j] == '?'
+		        || usr.password[j] == '¡' || usr.password[j] == '!' || usr.password[j] == '-' || usr.password[j] == ' '){
+		        				err = 1;
+								system("cls");
+								printf("\n\n\n**************************************************************************\n");
+								printf("--> ERROR: La contraseña no debe tener signos de puntuacion, ni espacios.\n");
+								printf("**************************************************************************\n\n\n\n\n");
+								system("pause");
+					}	
+				}
+			}
+		} while (err == 1);
+		
+		system("cls");
+		printf("\n\n----------------------------------------------------------------------------\n\n\n");
+		printf("*-*-*-*-*-*-            Registro de nuevo Asistente             -*-*-*-*-*-*\n\n\n");
+		printf("----------------------------------------------------------------------------\n\n\n\n");
+		SetConsoleTextAttribute(hConsole, 15);
+		printf("--> Ingrese el apellido y nombre: ");
 		_flushall();
 		gets(usr.name); 
 		fwrite(&usr, sizeof(user), 1, users);
